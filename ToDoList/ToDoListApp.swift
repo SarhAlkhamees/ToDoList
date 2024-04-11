9a93d045b8373e6cf0d70ef51aaee46524a83186
SarhAlkhamees <sarhsalkhamees@gmail.com>
2024-04-11 15:01:26 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sarah Alkhamees on 02/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
